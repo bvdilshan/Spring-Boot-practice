f441a68145f188269ab817f1aecb40ca4fb1e6d1
bvdilshan <vinothdilshanbvd@gmail.com>
2026-02-10 23:13:15 +0530

    Refactor CustomerController to include proper request mapping annotations for CRUD operations

--- src/main/java/com/example/posapi/api/CustomerController.java
+++ src/main/java/com/example/posapi/api/CustomerController.java
@@ -1,21 +1,39 @@
 package com.example.posapi.api;
 
-import com.example.posapi.dto.request.CustomerRequestDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.posapi.dto.request.CustomerRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
-    public String saveCustomer(CustomerRequestDto dto) {
+    @PostMapping
+    public String saveCustomer(@RequestBody CustomerRequestDto dto)
+     {
         return "Customer saved successfully: " + dto;
     }
-    public String getCustomerById(String id) {
+    @GetMapping("/{id}")
+    public String getCustomerById(@PathVariable String id) {
         return "Customer details for ID: " + id;
     }
-    public String updateCustomer(String id, CustomerRequestDto dto) {
+    @PutMapping("/{id}")
+    public String updateCustomer(@PathVariable String id, @RequestBody CustomerRequestDto dto) {
         return "Customer with ID: " + id + " updated successfully with details: " + "{" + dto + "}";
     }
-    public String deleteCustomer(String id) {
+    @DeleteMapping("/{id}")
+    public String deleteCustomer(@PathVariable String id) {
         return "Customer with ID: " + id + " deleted successfully."; 
         
     }
+    @GetMapping
     public String getAllCustomers() {
         return "List of all customers.";
     }   
